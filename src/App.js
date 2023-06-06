be9c52c9dc7a690d72f752e9ed64d330ff3b7af4
Debanjan <debanjan.c14@gmail.com>
2023-06-06 20:44:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useRef, useState } from 'react';
+
+
+
 
 function App() {
+
+  const inputref = useRef(null)
+  const [image, setImage] = useState("")
+
+  const handleImageClick = () => {
+    inputref.current.click()
+  }
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0]
+    console.log(file)
+    setImage(e.target.files[0])
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+
+        <div onClick={handleImageClick}>
+          {/* <img src="./photo.jpg" alt="" /> */}
+          {image ? (
+            <img src={URL.createObjectURL(image)} alt='' />
+          ) : (
+            <img src='./photo.jpg' alt='' />
+          )} <br />
+          <button className='img-upload-button'>Upload</button>
+          <input type="file" ref={inputref} onChange={handleImageChange} style={{ display: "none" }} />
+        </div>
+        
+      </div>
+
+    </>
   );
 }
 
